873cdeaf3af494b0d3e0ad20aafca691b836eb45
Pieter <pieter.gobin@gmail.com>
2019-11-26 22:51:10 +0100

    pandas dataframe works now, analysis file added

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 import json
 import requests
 import pandas as pd
-from pandas.io.json import json_normalize
+
 
 
 def wbclimate(variable, timescale, countriesList, export= False):
-    dataset = []
+    dataset = pd.DataFrame()
     countries_done = []
     countries = countriesList
     url_base = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/" + variable + "/" + timescale + "/"
@@ -16,21 +16,26 @@ def wbclimate(variable, timescale, countriesList, export= False):
                 response = requests.get(url)
                 if response.ok:
                     data = json.loads(response.content)
-                    entry = dict(country=country, observations=data)
-                    dataset.append(entry)
+                    templist =[]
+                    columnlist =[]
+                    for entry in data:
+                        templist.append(entry.get("data"))
+                        columnlist.append(entry.get("year"))
+                    newdf = pd.DataFrame([templist], columns=columnlist, index=[country])
+                    dataset = dataset.append(newdf)
                     countries_done.append(country)
                     remaining = len(countriesList) - len(countries_done)
-                    print("done: " + country + " remaining: " + remaining)
+                    print("done: " , country , " remaining: " , remaining)
                 else:
                     response.raise_for_status()
             countries = [x for x in countriesList if x not in countries_done]
         except requests.exceptions.HTTPError:
             countries = [x for x in countriesList if x not in countries_done]
     else:
-        datapd = json_normalize(dataset)
+
         if export:
-            savefile(datapd, "dataset.json")
-        return datapd
+            dataset.to_csv("dataset.csv")
+        return dataset
 
 
 
