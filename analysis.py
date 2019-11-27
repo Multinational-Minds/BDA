b2975462c47e3d42eb91b865ccd7ebfaab37c474
Pieter <pieter.gobin@gmail.com>
2019-11-27 01:52:25 +0100

    the wbgetdata is not working yet, comments added to wbclilmate

---
+++ analysis.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+import wbdata
+
+import functions as f
+
+countries = f.openfile("iso3.txt")
+iso3 = []
+
+for country in countries:
+    append = dict.get(country, 'alpha-3')
+    iso3.append(append)
+
+
+#data = f.wbclimate("tas", "year", iso3, export=True)
+
+data2 = f.wbdataset('SM.POP.NETM', iso3, 1960, 2012)
+
--- functions.py
+++ functions.py
@@ -4,7 +4,14 @@ import pandas as pd
 
 
 
-def wbclimate(variable, timescale, countriesList, export= False):
+def wbclimate(variable, timescale, countriesList, export=False):
+    """ retrieve historical climate data
+
+    variable is either tas or pr
+    timescale is decade, year or month
+    countriesList is a list of iso 3 codes for countries you want to look up
+    export = True will export the retrieved data as a csv and will ask for a name in the console
+    """
     dataset = pd.DataFrame()
     countries_done = []
     countries = countriesList
@@ -16,8 +23,8 @@ def wbclimate(variable, timescale, countriesList, export= False):
                 response = requests.get(url)
                 if response.ok:
                     data = json.loads(response.content)
-                    templist =[]
-                    columnlist =[]
+                    templist = []
+                    columnlist = []
                     for entry in data:
                         templist.append(entry.get("data"))
                         columnlist.append(entry.get("year"))
@@ -25,7 +32,7 @@ def wbclimate(variable, timescale, countriesList, export= False):
                     dataset = dataset.append(newdf)
                     countries_done.append(country)
                     remaining = len(countriesList) - len(countries_done)
-                    print("done: " , country , " remaining: " , remaining)
+                    print("done: ", country, " remaining: ", remaining)
                 else:
                     response.raise_for_status()
             countries = [x for x in countriesList if x not in countries_done]
@@ -34,10 +41,47 @@ def wbclimate(variable, timescale, countriesList, export= False):
     else:
 
         if export:
-            dataset.to_csv("dataset.csv")
+            name = input('file name: ') + ".csv"
+            dataset.to_csv(name)
         return dataset
 
 
+def wbdataset(topic, countriesList="all", startdate=None, enddate=None):
+    dataset = pd.DataFrame()
+    countries_done = []
+    countries = countriesList
+    url = 'http://api.worldbank.org/countries/'
+    if startdate is None and enddate is None:
+        url_extension = '/indicators/' + topic + "?format=json&per_page=1000"
+    elif startdate == enddate:
+        url_extension = '/indicators/' + topic + "?date=" + startdate + "&format=json&per_page=1000"
+    else:
+        url_extension = '/indicators/' + topic + "?date=" + str(startdate) + ":" + str(enddate) + "&format=json&per_page=1000"
+
+    while len(countries) > 0:
+        for country in countries:
+            try:
+                url = url + country+url_extension
+                response = requests.get(url)
+                if response.ok:
+                    data = json.loads(response.content)
+                    templist = []
+                    columnlist = []
+                    for entry in data:
+                        templist.append(entry.get("value"))
+                        columnlist.append(entry.get("date"))
+                    newdf = pd.DataFrame([templist], columns=columnlist, index=[country])
+                    dataset = dataset.append(newdf)
+                    countries_done.append(country)
+                    remaining = len(countriesList) - len(countries_done)
+                    print("done: ", country, " remaining: ", remaining)
+                else:
+                    response.raise_for_status()
+                countries = [x for x in countriesList if x not in countries_done]
+            except requests.exceptions.HTTPError:
+                countries = [x for x in countriesList if x not in countries_done]
+    dataset = dataset.transpose()
+    return dataset
 
 
 def openfile(name):
