d431ba9b2f3a1d429d3b5b95b415a5ac8d502463
Pieter <pieter.gobin@gmail.com>
2019-12-14 18:41:11 +0100

    Cleaning is done, on to averaging
    Signed-off-by: Pieter <pieter.gobin@gmail.com>

--- dataset_cleaning.py
+++ dataset_cleaning.py
@@ -1,8 +1,7 @@
 '''This file should be used for cleaning datasets and joining them to make one large pandas dataframe'''
 import datetime
 
-from statsmodels.graphics.tsaplots import plot_acf
-import statsmodels.api as sm
+from numpy.ma import count
 
 import pandas as pd
 import numpy as np
@@ -17,19 +16,16 @@ rain = f.openfile("rain.h5")
 temperature = f.openfile("temperature.h5")
 total_pop = f.openfile("total population.h5")
 dataset = pd.concat(
-    [temperature, rain, migration, arable, forest, pop_growth, total_pop, greenhouse_gasses]).sort_index(axis=0)
+    [temperature, rain, migration, arable, pop_growth, total_pop]).sort_index(axis=0)
 "f.season(arable.transpose())"
 '''f.savefile(arable,"arrrrrable", csv=True)
 sm.graphics.tsa.plot_acf(arable, lags=40)
 plot_acf(arable.transpose())  '''
-
-dataset1 = dataset.drop(dataset.iloc[:,0:59], inplace = True, axis = 1)
-
+dataset.drop(dataset.iloc[:, 0:59], inplace=True, axis=1)
 year2007 = datetime.date(2007, 1, 1)
 dataset[year2007].replace('', np.nan, inplace=True)
 dataset.dropna(subset=[year2007], inplace=True)
-
-f.savefile(dataset, "test data", csv = True)
-
-
-
+for index, df in dataset.groupby(level=0):
+    if len(df.index) < 5:
+        dataset = dataset.drop(index, level=0)
+f.savefile(dataset, "test data", csv=True)
