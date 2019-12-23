12de6bc098b136ffb6590c8c038da8bd0e6b40eb
sebas <sebastiandanciu@gmail.com>
2019-12-24 00:36:24 +0100

    useful subset selection info! iloc and xs can be used together ex: iloc[0:2,:] selects all columns from 1960 to 1970 xs('tas', level = 1, axis = 1) selects the tas variables across all countries hope this helps :)

--- Regression analysis.py
+++ Regression analysis.py
@@ -1,8 +1,55 @@
 import functions as f
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # plots
+import seaborn as sns  # more plots
 
-data = f.openfile('data.csv')
+from dateutil.relativedelta import relativedelta  # working with dates with style
+from scipy.optimize import minimize  # for function minimization
 
-stacked = data.stack()
+import statsmodels.formula.api as smf  # statistics and econometrics
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
+import datetime
+import json
+import requests
 
-f.savefile(stacked, 'test', csv=True)
+from itertools import product  # some useful functions
+from tqdm import tqdm_notebook
+
+data = f.openfile('data.h5')
+dt = data.columns.levels
+
+""" da = list(data.items())
+print(da)
+
+dt = data.describe()
+print(dt) """
+
+
+
+
+
+x = data.xs('tas', level = 1, axis = 1).iloc[0,:].values.reshape(-1,1)
+y = data.xs('pr', level = 1, axis = 1).iloc[0,:].values.reshape(-1,1)
+
+plt.scatter(x, y, marker='o')
+plt.title('pr vs tas 1960')
+plt.xlabel('tas')
+plt.ylabel('pr')
+plt.show()
+
+"""tas = data.xs('tas', level = 1, axis = 1).iloc[0:4,:]
+yrs = data.iloc[0:4,:]
+
+print(tas)"""
+
+
+
+"""plt.scatter(x, y, marker='o')
+plt.title('pr vs tas')
+plt.xlabel('tas')
+plt.ylabel('pr')
+plt.show()"""
