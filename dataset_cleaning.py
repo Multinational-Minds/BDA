7d4bc30a68ae0df00fa943e09bc1b2221718d4b4
Pieter <pieter.gobin@gmail.com>
2019-12-25 16:53:29 +0100

    Data rework complete, cleaning done as well
    Signed-off-by: Pieter <pieter.gobin@gmail.com>

--- dataset_cleaning.py
+++ dataset_cleaning.py
@@ -5,54 +5,34 @@ import numpy as np
 import pandas as pd
 import functions as f
 
+countries = f.openfile("iso3.txt")
+iso3 = []
+for country in countries:
+    append = dict.get(country, 'alpha-3')
+    iso3.append(append)
+
 arable = f.openfile("arable land.h5")
 migration = f.openfile("migration.h5")
 pop_growth = f.openfile("population growth.h5")
 rain = f.openfile("rain.h5")
 temperature = f.openfile("temperature.h5")
 total_pop = f.openfile("total population.h5")
-dataset = pd.concat(
-    [temperature, rain, migration, arable, pop_growth, total_pop], sort=True)
+
+varlist = [arable, temperature, pop_growth, rain, total_pop]
+dataset = migration
+for var in varlist:
+    dataset = pd.merge(dataset, var, on=['year', 'country'])
 dataset.columns = dataset.columns.str.replace('%', '%25')
 
-dataset2 = pd.DataFrame()
-datalist = [temperature, rain, migration, arable, pop_growth, total_pop]
-for data in datalist:
-    dataset2 = dataset2.append(data)
-dataset2.columns = dataset2.columns.str.replace('%', '%25')
-
-dataset.drop(dataset.iloc[:, 0:59], inplace=True, axis=1)
-year2007 = datetime.date(2007, 1, 1)
-dataset[year2007].replace('', np.nan, inplace=True)
-dataset.dropna(subset=[year2007], inplace=True)
-for index, df in dataset.groupby(level=0):
-    if len(df.index) < 5:
-        dataset = dataset.drop(index, level=0)
-
-rownames = list(dataset.index.values)
-rownames = rownames[0::5]
-count = 5
-previouscount = 0
 data = pd.DataFrame()
-for name in rownames:
-    series = dataset[previouscount:count]
-    count = count + 5
-    previouscount = previouscount + 5
-    series = series.mean()
-    frame = {name: series}
-    tempdf = pd.DataFrame(frame)
-    data = pd.concat([data, tempdf], axis=1)
+for country in iso3:
+    append = dataset.loc[dataset['country'] == str(country)]
+    append = append.sort_values(by='year')
+    append = append.groupby(np.arange(len(append)) // 5).agg(
+        {'year': 'first', 'country': 'last', 'Net migration': 'mean', 'tas': 'mean', 'pr': 'mean',
+         'Arable land (%25 of land area)': 'mean', 'Population growth (annual %25)': 'mean',
+         'Population, total': 'mean'})
+    append = append.dropna()
+    data = data.append(append, ignore_index=True)
 
-data.replace('', np.nan, inplace=True)
-data = data.dropna()
-for index, df in data.groupby(level=0):
-    if len(df.index) < 6:
-        data = data.drop(index, level=0)
-
-data = data.transpose()
-f.savefile(data, "data", csv=False)
-
-
-
-
-
+f.savefile(dataset, "data", csv=False)
